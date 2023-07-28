d1330846b9c85190274b68b07ae4ad4682ce7b70
Andrey <andrey.marus@gmail.com>
2023-07-28 06:34:28 +0300

    tests added

--- tests/gendiffTest.php
+++ tests/gendiffTest.php
@@ -9,7 +9,7 @@ class GenDiffTest extends TestCase
 {
     public function testGenDiff(): void
     {
-    $expected = "{
+    $expected1 = "{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -17,7 +17,53 @@ class GenDiffTest extends TestCase
   + timeout: 20
   + verbose: true
 }";
-    $this->assertEquals($expected, genDiff('./src/file1.json', './src/file2.json'));
-    $this->assertEquals($expected, genDiff('./src/file1.yml', './src/file2.yml'));
+    $this->assertEquals($expected1, genDiff('./src/file1.json', './src/file2.json'));
+    $this->assertEquals($expected1, genDiff('./src/file1.yml', './src/file2.yml'));
+    $expected2 = "{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}";
+    $this->assertEquals($expected2, genDiff('./src/file3.json', './src/file4.json'));
+    $this->assertEquals($expected2, genDiff('./src/file3.yml', './src/file4.yml'));
     }
 }
