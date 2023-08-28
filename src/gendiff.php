1d5abb72e0b87146e8b91cfb919d910907777b00
Andrey <andrey.marus@gmail.com>
2023-08-28 10:42:13 +0300

    Gendiff array2string - failure2

--- src/gendiff.php
+++ src/gendiff.php
@@ -9,6 +9,9 @@ use Symfony\Component\Yaml\Yaml;
 const SPACE = '    ';
 const ADD = '  + ';
 const SUB = '  - ';
+// const SPACE = 'SPC';
+// const ADD = 'ADD';
+// const SUB = 'SUB';
 
 function strbool($value)
 {
@@ -56,23 +59,23 @@ function checkArraysDifferences(array $arr1, array $arr2)
         ) {
             $comparison = checkArraysDifferences($arr1[$key], $arr2[$key]);
             var_dump($key);
-            $subResult[SPACE . $key] = $comparison;
+            $subResult[$key][SPACE] = [$comparison];
         } elseif (!array_key_exists($key, $arr2)) {
             // данные только в первом массиве
-            $subResult[SUB . $key] = $arr1[$key];
+            $subResult[$key][SUB] = [$arr1[$key]];
         } elseif (!array_key_exists($key, $arr1)) {
             // данные только во втором массиве
-            $subResult[ADD . $key] = $arr2[$key];
+            $subResult[$key][ADD] = [$arr2[$key]];
         } elseif (($arr1[$key] !== $arr2[$key])) {
             // оба массива содержат данные по ключу и данные - разные значения
-            $subResult[SUB . $key] = $arr1[$key];
-            $subResult[ADD . $key] = $arr2[$key];
+            $subResult[$key][SUB] = [$arr1[$key]];
+            $subResult[$key][ADD] = [$arr2[$key]];
         } else {
             // оба массива содержат данные по ключу и данные - одинаковые значения
-            $subResult[SPACE . $key] = $arr1[$key];
+            $subResult[$key][SPACE] = [$arr1[$key]];
         }
     }
-    // var_dump($subResult);
+    var_dump($subResult);
     return $subResult;
 }
 
@@ -91,37 +94,49 @@ function processArray($subValue, $level)
     $subResult = '';
     $newString = '';
     $keysArray = array_keys($subValue);
-    foreach ($keysArray as $key) {
-        if (!is_array($subValue[$key])) {
-            $newString = str_repeat(SPACE, $level) . $key . ": " . processValue($subValue[$key]) . "\n";
+    // foreach ($keysArray as $key) {
+    //    if (!is_array($subValue[$key])) {
+    // $newString = str_repeat(SPACE, $level) . $key . ": " . processValue($subValue[$key]) . "\n";
 //             $subResult .= str_repeat(SPACE, $level) . $newString;
-            $subResult .= $newString;
-        } else {
-            $level++;
-            $subResult .= str_repeat(SPACE, $level) . $key . ": {\n";
-            $subResult .= processArray($subValue[$key], $level);
-            $subResult .= str_repeat(SPACE, $level) . "}\n";
+    //        $subResult .= $newString;
+    //    } else {
+    //        $level++;
+    //        $subResult .= str_repeat(SPACE, $level) . $key . ": {\n";
+    //        $subResult .= processArray($subValue[$key], $level);
+    //        $subResult .= str_repeat(SPACE, $level) . "}\n";
+    //    }
+    //}
+    foreach ($keysArray as $key => $value) {
+        foreach ($value as $subKey => $subValue) {
+            var_dump($subKey);
+            if (!is_array($subValue[$subkey])) {
+                $subResult .= $subkey . $key . ": " . processValue($subValue[$subkey]) . "\n";
+            }
         }
     }
+    var_dump($subResult);
     return $subResult;
 }
 
 function stringify($value)
 {
     $level = 1;
-    $spacer = str_repeat(SPACE, $level);
+    // $spacer = str_repeat(SPACE, $level);
     if (!is_array($value)) {
-        $result = str_repeat(SPACE, $level) . processValue($value);
+//         $result = str_repeat(SPACE, $level) . processValue($value);
+        $result = processValue($value);
     } else {
         $keysArray = array_keys($value);
+        var_dump($keysArray);
         $result = "{\n";
         foreach ($keysArray as $key) {
             if (!is_array($value[$key])) {
                 $result .= $key . ": " . processValue($value[$key]) . "\n";
             } else {
-                $result .= $key . ": {\n";
-                $result .= str_repeat(SPACE, $level) . processArray($value[$key], $level);
-                $result .= str_repeat(SPACE, $level) . "}\n";
+        //        $result .= $key . ": {\n";
+        //        $result .= str_repeat(SPACE, $level) . processArray($value[$key], $level);
+        $result .= processArray($value[$key], $level);
+        //        $result .= str_repeat(SPACE, $level) . "}\n";
             }
         }
         $result .= "}";
